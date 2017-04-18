83a22d94bb6a3cd2c73ab41db23aaccd4db936d8
zhouyehua <zhouyehua@4417.com>
2017-04-18 10:30:51 +0800

    修改

---
+++ javaapk.com-GuanOWeather/src/com/example/heheweather/FragmentAndActivity.java
@@ -0,0 +1,11 @@
+package com.example.heheweather;
+
+import android.widget.EditText;
+
+
+public interface FragmentAndActivity {
+
+	public void senddata(EditText inputcity);
+	public void sendcitytext(String inputcitytext);
+	public void showDialog();
+}
